0d5cdda0a9627c31b0ff77dfcbda1682682626cb
Aaron Adams <aaron.adams@nccgroup.com>
2017-10-28 00:18:25 -0600

    testing multi-bin qcow2

--- unpack_repack_qcow2.sh
+++ unpack_repack_qcow2.sh
@@ -20,27 +20,60 @@ log()
 usage()
 {
     log "Usage:"
-    log "./unpack_repack_qcow2.sh -i <qcow2_file> [-o <out_qcow2_file> -t <template_qcow2_file> --inject-gdb --enable-gdb --disable-gdb --enable-aslr --disable-aslr --enable-root --disable-root --debug-shell --mount-qcow2 --unmount-qcow2 --unpack-only --grub-timeout"
+    log "./unpack_repack_qcow2.sh -i <qcow2_file> [-o <out_qcow2_file> -t <template_qcow2_file> --inject-gdb --enable-gdb --disable-gdb --enable-aslr --disable-aslr --enable-root --disable-root --debug-shell --mount-qcow2 --unmount-qcow2 --unpack-only --grub-timeout --inject-grub-config <grub.conf> --inject-multi-bin <multi-bin qcow2>"
     log "E.g.: ./unpack_repack_qcow2.sh -i /home/user/cisco/firmware/asav961-gns3.qcow2 -t /home/user/cisco/firmware/asav961.qcow2 -m -g -G -a -A -r -R -b"
     log "E.g.: ./unpack_repack_qcow2.sh -i /home/user/cisco/firmware/asav961-gns3.qcow2 -u"
+    log "E.g.: ./unpack_repack_qcow2.sh -i asav962-7-multiple-bins.qcow2 --inject-grub-conf grub-multi-bin.conf --inject-multi-bin asa962-7-smp-k8-noaslr-backdoor.bin"
     exit
 }
 
-# We inherit the name of the qcow2 and apply it to the bin, in case we have
-# duplicates and don't want to overwrite or binwalk the same file name.
-extract_bin()
+# Parameters:
+# 1 : String : qcow file
+# 2 : Integer: partition
+mount_qcow()
 {
     # Just avoid errors in case it's already connected
-    mount /dev/nbd0p1 ${1}
+    mount /dev/nbd0p${2} ${1}
     if [ $? != 0 ]; then
         log "[!] Could not mount ${1}"
         umount ${1}
         exit
     fi
-    log "Mounted /dev/nbd01 to ${1}"
+    log "Mounted /dev/nbd0p${2} to ${1}"
+}
+
+inject_grub_config()
+{
+    # In a mult-bin qcow partition 1 holds the grub config
+    mount_qcow ${1} 1
+    cp ${2} ${1}/boot/grub.conf
+    log "Overwrote ${1}/boot/grub/conf with ${2}"
+    umount ${1}
+}
+
+inject_multibin()
+{
+    # In a mult-bin qcow partition 2 holds the extra bin files
+    mount_qcow ${1} 2
+    cp ${2} ${1}/
+    # XXX - this is a test
+    cp config-asav9627 ${HOME}/mnt/qcow2/  
+    log "Wrote ${2} into partition 2 of multi-bin qcow"
+    umount ${1}
+}
+
+
+# We inherit the name of the qcow2 and apply it to the bin, in case we have
+# duplicates and don't want to overwrite or binwalk the same file name.
+extract_bin()
+{
+    # Assume a 1 partition qcow
+    mount_qcow ${1} 1
+
     BINPATH=$(ls ${1}/asa*.bin)
     if [ $? != 0 ]; then
         log "[!] Couldn't not find ${1}"
+        umount ${1}
         exit
     fi
     BIN=$(basename "${BINPATH}")
@@ -54,7 +87,7 @@ extract_bin()
         exit
     fi
     log "Copied ${BIN} to ${DEST}"
-    
+
     # XXX we do it here because after we unmount it. We should fix 
     # the architecture of this script so we only mount at the beginning 
     # and unmount at the end so we can do all modifications in between
@@ -140,6 +173,128 @@ add_serial()
     fini_nbd
 }
 
+### Actual workhorse logic ###
+
+# extract_qcow2(qcow2, mnt_dir, outfile)
+extract_qcow2()
+{
+    QNBD=$(which qemu-nbd)
+    if [ -z "${QNBD}" ]; then
+        log "[!] qemu-nbd tool not found. Please install or use -o"
+        exit
+    fi
+
+    init_nbd ${1}
+    if [ -z "${2}" -o -z "${3}" ]; then
+        log "[!] Extraction requires 3 arguments"
+        exit
+    else
+        extract_bin ${2} ${3}
+    fi
+    fini_nbd
+}
+
+# repackage_qcow2(binfile, repacked_name, new_qcow2, mntdir)
+repackage_qcow2()
+{
+    init_nbd ${3}
+    mount /dev/nbd0p1 ${4}
+    log "Mounted /dev/nbd01 to ${4}"
+    ORIG=$(ls ${4}/asa*.bin)
+    if [ $? != 0 ]; then
+        log "[!] Couldn't not find ${4}"
+        exit
+    fi
+    cp ${2} ${ORIG}
+    if [ $? != 0 ]; then
+        log "[!] Couldn't not find repacked name: ${2}"
+        exit
+    fi
+    log "Moved modified .bin inside of ${3}"
+    umount ${4}
+    fini_nbd
+    log "Unmounted ${4}"
+}
+
+extract_one()
+{
+    log "extract_one: $QCOW2FILE"
+
+    extract_qcow2 ${QCOW2FILE} ${QCOW2MNT} ${BINFILE}
+
+    # XXX - we generally want to avoid using -k as we want to keep the kernel to get the kernel version
+    # but we may want to support it in case we want to only keep the rootfs for debugging
+    #${UNPACK_REPACK_BIN} -i ${BINFILE} -u -k
+    ${UNPACK_REPACK_BIN} -i ${BINFILE} -u
+    FWFOLDER=${QCOWDIR}/bin/_${BASEQCOW2FILE_NOEXT}.qcow2.extracted
+    if [ ! -d "${FWFOLDER}" ]; then
+        log "Error: binwalk extraction failed. Didn't find ${FWFOLDER}"
+        exit
+    fi
+    mv ${FWFOLDER} ${QCOWDIR}/
+    if [[ "$DEBUG" == "NO" ]];
+    then
+    #rm ${BINFILE} ${BINFILE_REPACKED} ${BINFILE_REPACKED2}
+        rm ${BINFILE}
+    fi
+}
+
+extract_repack_one()
+{
+    log "extract_repack_one: $QCOW2FILE"
+
+    extract_qcow2 ${QCOW2FILE} ${QCOW2MNT} ${BINFILE}
+
+    ${UNPACK_REPACK_BIN} -i ${BINFILE} ${BIN_CMDLINE} -s
+    if [ $? != 0 ];
+    then
+        log ${UNPACK_REPACK_BIN} -i ${BINFILE} ${BIN_CMDLINE} -s failed
+        exit
+    fi
+
+    BINFILE_REPACKED=${QCOWDIR}/bin/${BASEQCOW2FILE_NOEXT}-repacked.qcow2
+    BINFILE_REPACKED2=${BINFILE_REPACKED}
+    if [[ "${ENABLE_GDB}" == " -g" ]] 
+    then
+        BINFILE_REPACKED=${QCOWDIR}/bin/${BASEQCOW2FILE_NOEXT}-repacked-gdbserver.qcow2
+        BINFILE_REPACKED2=${BINFILE_REPACKED}
+    elif [[ "$ENABLE_ROOT" == "YES" ]]
+    then
+        log "ENABLE ROOT"
+        BINFILE_REPACKED2=${QCOWDIR}/bin/${BASEQCOW2FILE_NOEXT}-repacked-rooted.qcow2
+        ${FWTOOL} -t -f ${BINFILE_REPACKED} -o ${BINFILE_REPACKED2}
+        if [ $? != 0 ];
+        then
+            log ${FWTOOL} -t -f ${BINFILE_REPACKED} -o ${BINFILE_REPACKED2} failed
+            exit
+        fi
+    elif [[ "$DISABLE_ROOT" == "YES" ]]
+    then
+        log "DISABLE ROOT"
+        BINFILE_REPACKED2=${QCOWDIR}/${BASEQCOW2FILE_NOEXT}-repacked-rooted.bin
+        ${FWTOOL} -T -f ${BINFILE_REPACKED} -o ${BINFILE_REPACKED2}
+        if [ $? != 0 ];
+        then
+            log ${FWTOOL} -T -f ${BINFILE_REPACKED} -o ${BINFILE_REPACKED_ROOTED} failed
+            exit
+        fi
+    fi
+
+    cp ${QCOW2FILE} ${OUTQCOW2FILE}
+    repackage_qcow2 ${BINFILE} ${BINFILE_REPACKED2} ${OUTQCOW2FILE} ${QCOW2MNT}
+
+    if [[ "$DEBUG" == "NO" ]];
+    then
+        if [[ "${BINFILE_REPACKED}" == "${BINFILE_REPACKED2}" ]]
+        then
+            rm ${BINFILE} ${BINFILE_REPACKED2}
+        else
+            rm ${BINFILE} ${BINFILE_REPACKED} ${BINFILE_REPACKED2}
+        fi
+    fi
+}
+
+
 ### INIT ###
 
 if [ -z "${ASATOOLS}" ]; then
@@ -179,6 +334,8 @@ DEBUGSHELL=
 DEBUG="NO"
 UNPACK_ONLY="NO"
 GRUB_TIMEOUT="NO"
+INJECT_GRUBCONFIG="NO"
+INJECT_MULTIBIN="NO"
 while [[ $# -gt 0 ]]
 do
     key="$1"
@@ -251,6 +408,14 @@ do
         GRUB_TIMEOUT="YES"
         shift # past argument
         ;;
+        --inject-grub-conf)
+        INJECT_GRUB_CONF="${2}"
+        shift # past argument
+        ;;
+        --inject-multi-bin)
+        INJECT_MULTI_BIN="${2}"
+        shift # past argument
+        ;;
         -h|*)
         # unknown option
         log "[!] Unknown option provided: $key"
@@ -298,131 +463,9 @@ then
     OUTQCOW2FILE=${QCOWDIR}/${BASEQCOW2FILE_NOEXT}-repacked.qcow2
 fi
 
-### Actual workhorse logic ###
-
-# extract_qcow2(qcow2, mnt_dir, outfile)
-extract_qcow2()
-{
-    QNBD=$(which qemu-nbd)
-    if [ -z "${QNBD}" ]; then
-        log "[!] qemu-nbd tool not found. Please install or use -o"
-        exit
-    fi
-
-    init_nbd ${1}
-    if [ -z "${2}" -o -z "${3}" ]; then
-        log "[!] Extraction requires 3 arguments"
-        exit
-    else
-        extract_bin ${2} ${3}
-    fi
-    fini_nbd
-}
-
-# repackage_qcow2(binfile, repacked_name, new_qcow2, mntdir)
-repackage_qcow2()
-{
-    init_nbd ${3}
-    mount /dev/nbd0p1 ${4}
-    log "Mounted /dev/nbd01 to ${4}"
-    ORIG=$(ls ${4}/asa*.bin)
-    if [ $? != 0 ]; then
-        log "[!] Couldn't not find ${4}"
-        exit
-    fi
-    cp ${2} ${ORIG}
-    if [ $? != 0 ]; then
-        log "[!] Couldn't not find repacked name: ${2}"
-        exit
-    fi
-    log "Moved modified .bin inside of ${3}"
-    umount ${4}
-    fini_nbd
-    log "Unmounted ${4}"
-}
-
-extract_one()
-{
-    log "extract_one: $QCOW2FILE"
-    
-    extract_qcow2 ${QCOW2FILE} ${QCOW2MNT} ${BINFILE}
-    
-    # XXX - we generally want to avoid using -k as we want to keep the kernel to get the kernel version
-    # but we may want to support it in case we want to only keep the rootfs for debugging
-    #${UNPACK_REPACK_BIN} -i ${BINFILE} -u -k
-    ${UNPACK_REPACK_BIN} -i ${BINFILE} -u
-    FWFOLDER=${QCOWDIR}/bin/_${BASEQCOW2FILE_NOEXT}.qcow2.extracted
-    if [ ! -d "${FWFOLDER}" ]; then
-        log "Error: binwalk extraction failed. Didn't find ${FWFOLDER}"
-        exit
-    fi
-    mv ${FWFOLDER} ${QCOWDIR}/
-    if [[ "$DEBUG" == "NO" ]];
-    then
-    #rm ${BINFILE} ${BINFILE_REPACKED} ${BINFILE_REPACKED2}
-        rm ${BINFILE}
-    fi
-}
-
-extract_repack_one()
-{
-    log "extract_repack_one: $QCOW2FILE"
-    
-    extract_qcow2 ${QCOW2FILE} ${QCOW2MNT} ${BINFILE}
-
-    ${UNPACK_REPACK_BIN} -i ${BINFILE} ${BIN_CMDLINE} -s
-    if [ $? != 0 ];
-    then
-        log ${UNPACK_REPACK_BIN} -i ${BINFILE} ${BIN_CMDLINE} -s failed
-        exit
-    fi
-
-    BINFILE_REPACKED=${QCOWDIR}/bin/${BASEQCOW2FILE_NOEXT}-repacked.qcow2
-    BINFILE_REPACKED2=${BINFILE_REPACKED}
-    if [[ "${ENABLE_GDB}" == " -g" ]] 
-    then
-        BINFILE_REPACKED=${QCOWDIR}/bin/${BASEQCOW2FILE_NOEXT}-repacked-gdbserver.qcow2
-        BINFILE_REPACKED2=${BINFILE_REPACKED}
-    elif [[ "$ENABLE_ROOT" == "YES" ]]
-    then
-        log "ENABLE ROOT"
-        BINFILE_REPACKED2=${QCOWDIR}/bin/${BASEQCOW2FILE_NOEXT}-repacked-rooted.qcow2
-        ${FWTOOL} -t -f ${BINFILE_REPACKED} -o ${BINFILE_REPACKED2}
-        if [ $? != 0 ];
-        then
-            log ${FWTOOL} -t -f ${BINFILE_REPACKED} -o ${BINFILE_REPACKED2} failed
-            exit
-        fi
-    elif [[ "$DISABLE_ROOT" == "YES" ]]
-    then
-        log "DISABLE ROOT"
-        BINFILE_REPACKED2=${QCOWDIR}/${BASEQCOW2FILE_NOEXT}-repacked-rooted.bin
-        ${FWTOOL} -T -f ${BINFILE_REPACKED} -o ${BINFILE_REPACKED2}
-        if [ $? != 0 ];
-        then
-            log ${FWTOOL} -T -f ${BINFILE_REPACKED} -o ${BINFILE_REPACKED_ROOTED} failed
-            exit
-        fi
-    fi
-
-    cp ${QCOW2FILE} ${OUTQCOW2FILE}
-    repackage_qcow2 ${BINFILE} ${BINFILE_REPACKED2} ${OUTQCOW2FILE} ${QCOW2MNT}
-
-    if [[ "$DEBUG" == "NO" ]];
-    then
-        if [[ "${BINFILE_REPACKED}" == "${BINFILE_REPACKED2}" ]]
-        then
-            rm ${BINFILE} ${BINFILE_REPACKED2}
-        else
-            rm ${BINFILE} ${BINFILE_REPACKED} ${BINFILE_REPACKED2}
-        fi
-    fi
-}
-
-
-# We exit immediately because this is used for patching a flash qcow2 and not
-# the same qcow2 for enabling gdb, etc.
 if [ ! -z "${ENABLE_SERIAL}" ]; then
+    # We exit immediately because this is used for patching a flash qcow2 and 
+    # not the same qcow2 for enabling gdb, etc.
     add_serial "${QCOW2FILE}" "${QCOW2MNT}"
     exit
 fi
@@ -443,22 +486,39 @@ if [ ! -z "${QCOW_UMOUNT}" ]; then
     exit
 fi
 
-log "Using input qcow2 file: ${QCOW2FILE}"
-log "Using template qcow2 file: ${TEMPLATEQCOW2FILE}"
-log "Using output qcow2 file: ${OUTQCOW2FILE}"
-log "Command line: ${BIN_CMDLINE}"
+if [[ ! -z "$INJECT_GRUB_CONF" && -z "$INJECT_MULTI_BIN" ]]; then
+    log "ERROR: grub.conf injection currently requires --inject-multi-bin"
+    exit
+fi
 
-# Work is done in a "bin" directory because we use the same name for the .bin
-# that for the .qcow2. This is for several reasons:
-# 1. our database contains actual .qcow2 name so we need this when patching "lina" to the right offsets
-# 2. binwalk will create a folder with the .bin name so we need it to also match the actual .qcow2 so
-#    it is correct when debugging
-mkdir ${QCOWDIR}/bin
-BINFILE=${QCOWDIR}/bin/${BASEQCOW2FILE_NOEXT}.qcow2
+if [[ ! -z "$INJECT_MULTI_BIN" ]]; then
+    init_nbd ${INJECT_MULTI_BIN}
+    if [[ ! -z "${INJECT_GRUB_CONF}" ]]; then
+        echo "Injecting grub config"
+        inject_grub_config ${QCOW2MNT} ${INJECT_GRUB_CONF}
+    fi
+    log "Injecting ${QCOW2FILE} into ${OUTQCOW2FILE}"
+    inject_multibin ${QCOW2MNT} ${QCOW2FILE}
 
-if [[ "$UNPACK_ONLY" == "YES" ]]
-then
-    extract_one
+    fini_nbd
 else
-    extract_repack_one
+    log "Using input qcow2 file: ${QCOW2FILE}"
+    log "Using template qcow2 file: ${TEMPLATEQCOW2FILE}"
+    log "Using output qcow2 file: ${OUTQCOW2FILE}"
+    log "Command line: ${BIN_CMDLINE}"
+
+    # Work is done in a "bin" directory because we use the same name for the .bin
+    # that for the .qcow2. This is for several reasons:
+    # 1. our database contains actual .qcow2 name so we need this when patching "lina" to the right offsets
+    # 2. binwalk will create a folder with the .bin name so we need it to also match the actual .qcow2 so
+    #    it is correct when debugging
+    mkdir ${QCOWDIR}/bin
+    BINFILE=${QCOWDIR}/bin/${BASEQCOW2FILE_NOEXT}.qcow2
+
+    if [[ "$UNPACK_ONLY" == "YES" ]]
+    then
+        extract_one
+    else
+        extract_repack_one
+    fi
 fi
